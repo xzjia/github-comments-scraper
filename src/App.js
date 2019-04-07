2658484449bc886d093d79a0dd2e9d346d973dc5
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2019-04-07 22:27:07 +0900

    Enable CSV download of comments.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Button, Form, Card, Container} from 'react-bootstrap';
-import LoadingOverlay from 'react-loading-overlay'
-import {PacmanLoader} from 'react-spinners'
+import LoadingOverlay from 'react-loading-overlay';
+import {PacmanLoader} from 'react-spinners';
+import { CSVLink } from "react-csv";
+
 import 'bootstrap/dist/css/bootstrap.css';
 
 const Octokit = require('@octokit/rest');
@@ -35,6 +37,13 @@ class App extends Component {
             } else {
               return true;
             }
+          }).map(raw_comment => {
+            return {
+              "created_at": raw_comment.created_at,
+              "body": raw_comment.body,
+              "html_url": raw_comment.html_url,
+              "user": "login" in raw_comment.user && raw_comment.user.login,
+            }
           }),
           loading: false
         })
@@ -54,7 +63,7 @@ class App extends Component {
         <Form>
           <Form.Group controlId="githubToken">
             <Form.Label>Github Token</Form.Label>
-            <Form.Control type="text" name="githubToken" onChange={this.handleInputChange} placeholder="Github Access Token" />
+            <Form.Control type="text" name="githubToken" onChange={this.handleInputChange} placeholder="Github Access Token, if none is given, then only public repositories will be accessible." />
           </Form.Group>
           <Form.Group controlId="targetRepo">
             <Form.Label>Target repository</Form.Label>
@@ -62,15 +71,16 @@ class App extends Component {
           </Form.Group>
           <Form.Group controlId="targetUser">
             <Form.Label>Target user name</Form.Label>
-            <Form.Control type="text" name="targetUser" onChange={this.handleInputChange} placeholder="Target user name, e.g. xzjia" />
+            <Form.Control type="text" name="targetUser" onChange={this.handleInputChange} placeholder="Target user name, e.g. xzjia. If none is given, all comments will show up." />
           </Form.Group>
           <Button onClick={this.handleClick} variant="primary">Submit</Button>
+          {this.state.comments.length > 0 && <CSVLink data={this.state.comments}>Download</CSVLink>}
         </Form>
         {this.state.comments.map((comment, index) => (
           <Card key={index} style={{ width: '18rem' }}>
             <Card.Body>
               <Card.Title>Review Comment</Card.Title>
-              <Card.Subtitle>{comment.created_at}</Card.Subtitle>
+              <Card.Subtitle>{comment.user} at {comment.created_at}</Card.Subtitle>
               <Card.Text>{comment.body}</Card.Text>
               <Card.Link href={comment.html_url}>Link</Card.Link>
             </Card.Body>
